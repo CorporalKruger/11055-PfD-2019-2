82a41b0281d60c9b717b643d51ded69baa8cc4e8
Kiandra <kma6281@gmail.com>
2019-09-09 10:18:20 +1000

    Week 6

---
+++ Kiandra/sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+
+//creating the backgound
+createCanvas(400, 400);
+background(250);
+
+//variable for number of eyes
+var eyes = prompt("How many eyes will I have today?", "1-3");
+var txtr = ""
+}
+
+function draw() {
+    
+    fill (243,211,191);
+    ellipse(200, 150, 100, 100);
+    
+    
+if (eyes == null||eyes == ""){
+    text = "User cancelled prompt"
+}else if (eyes == 1){
+    ellipse ()
+}
+}
