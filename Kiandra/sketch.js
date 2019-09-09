e832d5cc425e110d2b057e4858700722038a7a29
Kiandra <kma6281@gmail.com>
2019-09-09 11:14:59 +1000

    Update sketch.js

--- Kiandra/sketch.js
+++ Kiandra/sketch.js
@@ -30,27 +30,39 @@ function draw() {
 switch (eyes){
     
     case "1":
+    fill (255);
+    ellipse (200, 130, 30, 30);
     fill (0);
-    ellipse (200, 140, 20, 20);
+    ellipse (200, 130, 20, 20);
     break;
 
     case "2":
+    fill (255);
+    ellipse (180, 145, 30, 30);
     fill (0);
     ellipse (180, 145, 20, 20);
     
+    fill(255);
+    ellipse (220, 145, 30, 30);
     fill (0);
     ellipse (220, 145, 20, 20);
     break;
     
     case "3":
+    fill (255);
+    ellipse (180, 145, 25, 25);
     fill (0);
-    ellipse (180, 145, 20, 20);
+    ellipse (180, 145, 15, 15);
     
+    fill (255);
+    ellipse (220, 145, 25, 25);
     fill (0);
-    ellipse (220, 145, 20, 20);
+    ellipse (220, 145, 15, 15);
     
-    fill(0);
-    ellipse (200, 130, 20, 20);
+    fill (255);
+    ellipse (200, 120, 25, 25);
+    fill (0);
+    ellipse (200, 120, 15, 15);
     break;
 }
 }
